d729ec39add34441720680ac3cea756ee78a9517
Thallia <85205473+TM-Souza@users.noreply.github.com>
2021-10-30 12:37:46 -0300

    Add files via upload

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -3,8 +3,8 @@
 //CONEXÃO COM O BANCO DE DADOS
 
 $servername = "localhost";
-$username = "thalliamichelle
-$password = "13x27x41";
+$username = "root";
+$password = "";
 $db_name = "loja_carros";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
